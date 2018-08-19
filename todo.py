b286a0b184892aa099d21098715a992ca8ee6436
AhmerZunair <30389585+AhmerZunair@users.noreply.github.com>
2018-08-19 14:09:28 -0700

    Add files via upload

--- todo.py
+++ todo.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify,json,request,render_template
 from flask_pymongo import PyMongo
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required, Length
-from wtforms import Form, StringField, SubmitField
+from wtforms import Form, StringField, SubmitField, DecimalField
 
 
 app = Flask(__name__)
@@ -13,12 +13,16 @@ mongo = PyMongo(app)
 
 
 class InsertForm(Form):
+    id = DecimalField(
+        'id', validators=[Required(), Length(1, 100)]
+    )
     title = StringField(
         'title', validators=[Required(), Length(1, 100)]
     )
     description = StringField(
         'description', validators=[Required(), Length(1, 100)]
     )
+
     submit = SubmitField('submit')
 class searchForm(Form):
     title=StringField('title',validators=[Required()])
@@ -33,8 +37,8 @@ def home():
 
 @app.route("/todo/api/v1.0/",methods=['GET'])
 def index():
- 
-    data = mongo.db.todo.find({}).count()
+    todo = mongo.db.todo
+    data = todo.find({}).count()
     if (data == 0):
         return "data not found"
     else:
@@ -42,7 +46,7 @@ def index():
         data=mongo.db.todo.find({})
         for i in data:
 
-            d[i['id']] = {"id": i["id"],"title": i["title"],"description":i["description"],    "done": bool(i["done"])}
+            d[i['id']] = {"id": i["id"],"title": i["title"],"description":i["description"],"done": bool(i["done"])}
         
         return jsonify(d)
 
@@ -51,12 +55,13 @@ def index():
 def Insert():
     form=InsertForm(request.form)
     if request.method=='POST':
+       id = request.form['id']
        title=request.form['title']
        description=request.form['description']
        done = bool(request.form["submit"])
        count=mongo.db.todo.find({}).count()
        
-       mongo.db.todo.insert({'id':count+1,'title':title,'description':description,'done':done })
+       mongo.db.todo.insert({'id':id,'title':title,'description':description,'done':done })
        return "Successfully add"
                   
        
@@ -67,8 +72,8 @@ def search():
     form=searchForm(request.form)
     if request.method=='POST':
       
-       if mongo.db.todo.find({'title':request.form['title']}):
-           record=mongo.db.todo.find({'title':request.form['title']}) 
+       if mongo.db.todo.find({'id':request.form['id']}):
+           record=mongo.db.todo.find({'id':request.form['id']})
            d=[]
            for i in record:
               d.append({'id':i['id'],'title':i['title'],'description':i['description'],'done':i['done'] })
@@ -82,12 +87,13 @@ def search():
 
 def update():
     if request.method=='POST':
-        title1=request.form['title1']
-        title2=request.form['title2']
+        id1=request.form['id1']
+        id2=request.form['id2']
+        title = request.form['title']
         description=request.form['description']
         done = bool(request.form["submit"])
-        if mongo.db.todo.find({'title':title1}):    
-            update_query = mongo.db.todo.update_one({'title':title1},{'$set':{"title": title2,"description": description,"done": done}})
+        if mongo.db.todo.find({'id':id1}):
+            update_query = mongo.db.todo.update_one({'id':id1},{'$set':{"id": id2,"title": title,"description": description,"done": done}})
             return ("successfully updated.")
         else: 
            return 'title does not exist' 
@@ -102,8 +108,8 @@ def update():
 @app.route("/todo/api/v1.0/delete", methods=['GET','POST','DELETE'])
 def delete():
     if request.method == 'POST':
-        title = request.form['title']
-        mongo.db.todo.delete_one({'title': title})
+        id = request.form['id']
+        mongo.db.todo.delete_one({'id': id})
         return "deleted"
         
 
